38f66a30e41a30dbe5d6066696abbdd634cb84e8
Tung090505 <2331540153@vaa.edu.vn>
2025-11-03 11:37:06 +0700

    Ui

---
+++ Azura Land/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace GameScripts
+{
+    public class BGManager : MonoBehaviour
+    {
+        public Image bgTraiDat;
+        public Image bgNamec;
+        public Image bgXayda;
+
+        public CharacterManager characterManager; // kéo CharacterManager vào Inspector
+
+        private Image currentBG;
+
+        void Start()
+        {
+            currentBG = bgTraiDat;
+            SetAlpha(bgTraiDat, 1);
+            SetAlpha(bgNamec, 0);
+            SetAlpha(bgXayda, 0);
+        }
+
+        public void ShowPlanet(string planet)
+        {
+            Image nextBG = currentBG;
+            switch (planet)
+            {
+                case "TraiDat": nextBG = bgTraiDat; break;
+                case "Namec": nextBG = bgNamec; break;
+                case "Xayda": nextBG = bgXayda; break;
+            }
+
+            if (nextBG != currentBG)
+                StartCoroutine(SwitchBackground(nextBG));
+
+            // Hiển thị nhân vật cho planet
+            if (characterManager != null)
+                characterManager.ShowCharactersForPlanet(planet);
+        }
+
+        IEnumerator SwitchBackground(Image nextBG)
+        {
+            float duration = 0.8f;
+            float t = 0f;
+            Image prevBG = currentBG;
+
+            while (t < duration)
+            {
+                t += Time.deltaTime;
+                float alpha = t / duration;
+                SetAlpha(prevBG, 1 - alpha);
+                SetAlpha(nextBG, alpha);
+                yield return null;
+            }
+
+            SetAlpha(prevBG, 0);
+            SetAlpha(nextBG, 1);
+            currentBG = nextBG;
+        }
+
+        void SetAlpha(Image img, float alpha)
+        {
+            if (img == null) return;
+            Color c = img.color;
+            c.a = Mathf.Clamp01(alpha);
+            img.color = c;
+        }
+    }
+}
---
+++ Azura Land/Assets/Scripts/CharacterCustomizer.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameScripts
+{
+    public class CharacterManager : MonoBehaviour
+    {
+        [Header("UI")]
+        public GameObject panelCharacter;
+        public Button btnChar1;
+        public Button btnChar2;
+        public Button btnDoiToc;
+        public Image imgCharacter;
+
+        [Header("Sprites")]
+        public Sprite gohanBody;
+        public Sprite yamchaBody;
+        public Sprite ocTieuBody;
+        public Sprite kamiBody;
+        public Sprite radicBody;
+        public Sprite kakalotBody;
+
+        [Header("Tóc")]
+        public Sprite hair1;
+        public Sprite hair2;
+
+        private Sprite currentHair;
+        private bool usingHair1 = true;
+        private string currentPlanet = "";
+
+        void Start()
+        {
+            panelCharacter.SetActive(true);
+        }
+
+        public void ShowCharactersForPlanet(string planet)
+        {
+            panelCharacter.SetActive(true);
+            currentPlanet = planet;
+
+            btnChar1.onClick.RemoveAllListeners();
+            btnChar2.onClick.RemoveAllListeners();
+
+            switch (planet)
+            {
+                case "TraiDat":
+                    btnChar1.onClick.AddListener(() => SelectCharacter(gohanBody));
+                    btnChar2.onClick.AddListener(() => SelectCharacter(yamchaBody));
+                    break;
+                case "Namec":
+                    btnChar1.onClick.AddListener(() => SelectCharacter(ocTieuBody));
+                    btnChar2.onClick.AddListener(() => SelectCharacter(kamiBody));
+                    break;
+                case "Xayda":
+                    btnChar1.onClick.AddListener(() => SelectCharacter(radicBody));
+                    btnChar2.onClick.AddListener(() => SelectCharacter(kakalotBody));
+                    break;
+            }
+        }
+
+        public void SelectCharacter(Sprite body)
+        {
+            imgCharacter.sprite = body;
+            imgCharacter.color = Color.white;
+            currentHair = hair1;
+            usingHair1 = true;
+        }
+
+        public void DoiToc()
+        {
+            if (imgCharacter.sprite == null) return;
+
+            usingHair1 = !usingHair1;
+            currentHair = usingHair1 ? hair1 : hair2;
+            imgCharacter.sprite = currentHair;
+        }
+    }
+}
