6abac4c5c04c2c3e0e832101229e0d4d4804f6ae
Tung090505 <2331540153@vaa.edu.vn>
2025-11-09 20:13:05 +0700

    done CreatePLayer

--- Azura Land/Assets/Scripts/CharacterCustomizer.cs
+++ Azura Land/Assets/Scripts/CharacterCustomizer.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using TMPro; // 👈 thêm dòng này
+using TMPro;
 
 namespace GameScripts
 {
@@ -10,93 +10,77 @@ namespace GameScripts
         public GameObject panelCharacter;
         public Button btnChar1;
         public Button btnChar2;
-        public Button btnDoiToc;
 
-        [Header("Ảnh nhân vật")]
-        public Image imgBody;
-        public Image imgHair;
+        [Header("Prefab nhân vật - Trái Đất")]
+        public GameObject gohanPrefab;
+        public GameObject yamchaPrefab;
 
-        [Header("Sprites thân - Trái Đất")]
-        public Sprite gohanBody;
-        public Sprite yamchaBody;
+        [Header("Prefab nhân vật - Namec")]
+        public GameObject ocTieuPrefab;
+        public GameObject kamiPrefab;
 
-        [Header("Sprites thân - Namec")]
-        public Sprite ocTieuBody;
-        public Sprite kamiBody;
+        [Header("Prefab nhân vật - Xayda")]
+        public GameObject radicPrefab;
+        public GameObject kakalotPrefab;
 
-        [Header("Sprites thân - Xayda")]
-        public Sprite radicBody;
-        public Sprite kakalotBody;
-
-        [Header("Sprites tóc")]
-        public Sprite hair1;
-        public Sprite hair2;
-
-        private bool usingHair1 = true;
-        private string currentPlanet = "";
-        private Sprite currentBody;
+        private GameObject currentCharacter;
 
         void Start()
         {
-            if (btnDoiToc != null)
-                btnDoiToc.onClick.AddListener(DoiToc);
+            panelCharacter.SetActive(false);
         }
 
+        // 🔹 Gọi khi chọn hành tinh
         public void ShowCharactersForPlanet(string planet)
         {
-            currentPlanet = planet;
             panelCharacter.SetActive(true);
 
+            // Xóa sự kiện click cũ để tránh lỗi
             btnChar1.onClick.RemoveAllListeners();
             btnChar2.onClick.RemoveAllListeners();
 
-            // 👉 Dùng TextMeshProUGUI thay vì Text
+            // Lấy Text hiển thị trên nút
             TextMeshProUGUI txt1 = btnChar1.GetComponentInChildren<TextMeshProUGUI>();
             TextMeshProUGUI txt2 = btnChar2.GetComponentInChildren<TextMeshProUGUI>();
 
+            // Gán prefab & tên theo hành tinh
             switch (planet)
             {
                 case "TraiDat":
-                    SetupButton(btnChar1, gohanBody, "Gohan", txt1);
-                    SetupButton(btnChar2, yamchaBody, "Yamcha", txt2);
+                    SetupButton(btnChar1, gohanPrefab, "Gohan", txt1);
+                    SetupButton(btnChar2, yamchaPrefab, "Yamcha", txt2);
                     break;
 
                 case "Namec":
-                    SetupButton(btnChar1, ocTieuBody, "Ốc tiêu", txt1);
-                    SetupButton(btnChar2, kamiBody, "Kami", txt2);
+                    SetupButton(btnChar1, ocTieuPrefab, "Ốc Tiêu", txt1);
+                    SetupButton(btnChar2, kamiPrefab, "Kami", txt2);
                     break;
 
                 case "Xayda":
-                    SetupButton(btnChar1, radicBody, "Radic", txt1);
-                    SetupButton(btnChar2, kakalotBody, "Kakalot", txt2);
+                    SetupButton(btnChar1, radicPrefab, "Radic", txt1);
+                    SetupButton(btnChar2, kakalotPrefab, "Kakalot", txt2);
                     break;
             }
         }
 
-        private void SetupButton(Button btn, Sprite body, string name, TextMeshProUGUI txt)
+        // 🔹 Gán sự kiện click đúng chuẩn
+        private void SetupButton(Button btn, GameObject prefab, string name, TextMeshProUGUI txt)
         {
             if (txt != null)
-                txt.text = name; // đổi chữ trên nút
+                txt.text = name;
 
-            btn.onClick.AddListener(() => SelectCharacter(body));
+            btn.onClick.AddListener(() => SelectCharacter(prefab));
         }
 
-        public void SelectCharacter(Sprite body)
+        // 🔹 Khi click chọn nhân vật
+        public void SelectCharacter(GameObject prefab)
         {
-            currentBody = body;
-            imgBody.sprite = currentBody;
-            imgBody.color = Color.white;
+            // Xóa nhân vật cũ
+            if (currentCharacter != null)
+                Destroy(currentCharacter);
 
-            usingHair1 = true;
-            imgHair.sprite = hair1;
-            imgHair.color = Color.white;
-        }
-
-        public void DoiToc()
-        {
-            if (imgBody.sprite == null) return;
-            usingHair1 = !usingHair1;
-            imgHair.sprite = usingHair1 ? hair1 : hair2;
+            // Tạo nhân vật mới giữa màn hình
+            currentCharacter = Instantiate(prefab, new Vector3(0, -2, 0), Quaternion.identity);
         }
     }
 }
