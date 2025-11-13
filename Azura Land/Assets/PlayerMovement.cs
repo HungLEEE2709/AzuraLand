9f3a5021bbb5c7163797e5260885d8c8567386b7
Tung090505 <2331540153@vaa.edu.vn>
2025-11-13 20:28:18 +0700

    Player

---
+++ Azura Land/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using Quantum;
+using UnityEngine;
+
+public class GohanView : MonoBehaviour
+{
+    public QuantumEntityView view;
+    private Animator animator;
+
+    private GameObject idle;
+    private GameObject run;
+    private GameObject kame;
+    private GameObject hit;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        idle = transform.Find("Idle").gameObject;
+        run = transform.Find("Run").gameObject;
+        kame = transform.Find("Kame").gameObject;
+        hit = transform.Find("Hit").gameObject;
+    }
+
+    void Update()
+    {
+        if (view == null || view.EntityRef == EntityRef.None)
+            return;
+
+        var f = QuantumRunner.DefaultGame.Frames.Predicted;
+
+
+    }
+}
